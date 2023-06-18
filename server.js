89ab28c2544d296f4bf72ca49c6b5adb11d12207
Matias Muhonen <matiasj.saarinen@gmail.com>
2023-06-18 20:20:38 +0300

    Add path

--- server.js
+++ server.js
@@ -2,14 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
-  name = req.params["name"];
-  if (name) {
-    res.send(greeting + "</br>and hello to " + name);
-  } else {
-    res.send(greeting);
-  }
+app.get("/joulu/muistio", (req, res) => {
+  res.send("Well hello!")
 });
 
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
